035f105fcd410779cd4f9d8d551d5f2e664daf42
alex <kravz2702@gmail.com>
2021-07-10 00:46:38 +0300

    chapter 2
    chapter 2

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!!!"
+    user = {'username':'Alexandr'}
+    posts = [{
+    'author':{'username':'John'},
+    'body': 'Beautiful day in Portland'
+    },
+    {
+        'author':{'username':'Susan'},
+        'body': 'The Avengers movie was so cool'
+    },
+    {
+        'author':{'username':'Igor'},
+        'body': 'I hate fish!!!'
+    }]
+    return render_template('index.html', user=user, title='Home', posts=posts)
