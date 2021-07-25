e2359d15afb6dd16860d3ea2eff49bb30e36d548
alex <kravz2702@gmail.com>
2021-07-25 22:53:50 +0300

    register
    register

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired , ValidationError, Email, EqualTo
-form app.models import User  
+from app.models import User  
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
--- microblog/app/models.py
+++ microblog/app/models.py
@@ -10,14 +10,14 @@ def laod_user(id):
     return User.query.get(int(id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
-    def set_password_(self, password):
+    def set_password_hash(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password_hash(self, password):
