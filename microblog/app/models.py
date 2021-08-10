5bdcee0221412db562ceaac4fbdc2188e08c438a
alex <kravz2702@gmail.com>
2021-08-10 20:25:23 +0300

    reset_password
    changes in forms, models, routes etc.

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -4,6 +4,7 @@ from flask_login import UserMixin
 from app import login
 from werkzeug.security import check_password_hash, generate_password_hash
 from hashlib import md5
+from time import time
 
 @login.user_loader
 def laod_user(id):
@@ -58,6 +59,20 @@ class User(db.Model, UserMixin):
                 followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+
+    def get_reset_password(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password':self.id, 'exp':time()+expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,11 +1,15 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.forms import LoginForm, RegistrationForm, \
+    EditProfileForm, PostForm, ResetPasswordRequestForm
 from app.models import User, Post
 from flask_login import current_user, login_user, logout_user, login_required
 from app import db
 from werkzeug.urls import url_parse
 from datetime import datetime
+from app.email import send_password_reset_email
+from app.forms import ResetPasswordForm 
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -143,3 +147,31 @@ def explore():
     return render_template('index.html', title='Explore', 
                             posts=posts.items, next_url=next_url,
                             prev_url=prev_url)
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instrucrions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html', title='Reset Password', form=form)
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password_hash(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
