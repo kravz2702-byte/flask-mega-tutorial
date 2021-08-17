9ef518bf601a87487c886dcc10ee9307f216b087
alex <kravz2702@gmail.com>
2021-08-17 22:16:49 +0300

    restructure
    changed structure

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,22 +1,21 @@
-from app import db 
 from datetime import datetime
-from flask_login import UserMixin
-from app import login
-from werkzeug.security import check_password_hash, generate_password_hash
 from hashlib import md5
 from time import time
+from flask import current_app
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from app import db, login
 
-@login.user_loader
-def laod_user(id):
-    return User.query.get(int(id))
 
-
-followers = db.Table('followers',
+followers = db.Table(
+    'followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('followed_id', db.Integer,db.ForeignKey('user.id'))
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
 
-class User(db.Model, UserMixin):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -30,17 +29,20 @@ class User(db.Model, UserMixin):
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
-    def set_password_hash(self, password):
+    def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-    def check_password_hash(self, password):
-        return check_password_hash(self.password_hash,password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
-    
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
     def follow(self, user):
         if not self.is_following(user):
             self.followed.append(user)
@@ -60,30 +62,32 @@ class User(db.Model, UserMixin):
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
-    def get_reset_password(self, expires_in=600):
+    def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
-            {'reset_password':self.id, 'exp':time()+expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256')
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            current_app.config['SECRET_KEY'], algorithm='HS256')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
-            algorithms=['HS256'])['reset_password']
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    language = db.Column(db.String(5))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-
-
